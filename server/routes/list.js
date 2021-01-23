c58c19161a43af6ea755cf92524b342352ddb6ea
masonleonhart <mason.leonhart@gmail.com>
2021-01-23 14:54:29 -0600

    post request works from server

--- server/routes/list.js
+++ server/routes/list.js
@@ -2,4 +2,30 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool.js');
 
+// Create (POST)
+router.post('/', (req, res) => {
+    let newTask = req.body;
+    console.log('Adding task', newTask);
+
+    let queryText = `INSERT INTO "tasks" ("name", "description", "status")
+                    VALUES ($1, $2, 'In progress');`;
+    pool.query(queryText, [newTask.taskName, newTask.taskDescription]).then(result => {
+        console.log('Added new task successfully');
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log(`Error adding new task`, error);
+        res.sendStatus(500);
+    });
+});
+
+// Read (GET)
+// router.get('/', (req, res) => {
+//     console.log()
+// });
+
+// Update (PUT)
+
+
+// Delete (DELETE)
+
 module.exports = router;
