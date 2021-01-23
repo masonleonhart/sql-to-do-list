95de57ca36b625c77f92c82063ebe4828280b141
masonleonhart <mason.leonhart@gmail.com>
2021-01-23 15:41:54 -0600

    get works from client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,33 @@
 $(() => {
     console.log('JQ and JS');
+    getTasks();
 });
+
+function getTasks() {
+    console.log(`Retrieving data from DB`);
+
+    $.ajax({
+        method: 'GET',
+        url: '/list'
+    }).then(response => {
+        console.log('Retrieved data successfully');
+        appendData(response);
+    }).catch(error => {
+        console.log('Error in retrieving data', error.statusText);
+    });
+};
+
+function appendData(tasks) {
+    $('#viewTasks').empty();
+
+    for (const task of tasks) {
+        $('#viewTasks').append(`
+            <tr>
+                <td>${task.name}</td>
+                <td>${task.description}</td>
+                <td>${task.status}</td>
+                <td><Button>Complete</button><button>Delete</button></td>
+            </tr>
+        `);
+    };
+};
